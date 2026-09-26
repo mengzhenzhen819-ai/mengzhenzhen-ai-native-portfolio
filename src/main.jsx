0eb069593f291f49c596e10d6fedcf6c54414931
mengzhenzhen819-ai <mengzhenzhen819@gmail.com>
2026-09-26 17:47:30 +0800

    Rename matrix platform project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.jsx?v=incentive-bg-v6";
-import "./styles.css?v=incentive-bg-v6";
+import { App } from "./App.jsx?v=matrix-title-v7";
+import "./styles.css?v=matrix-title-v7";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
